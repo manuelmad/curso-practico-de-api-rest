e64ad11e85b00649c101011eac591c3e83270149
Manuel Araujo <manuelmad1981@gmail.com>
2022-11-29 10:58:04 -0400

    Final del curso

--- src/navigation.js
+++ src/navigation.js
@@ -104,6 +104,10 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.add('inactive');
     movieDetailSection.classList.remove('inactive');
+
+    const [ , movieId] = location.hash.split('=');
+
+    getMovieById(movieId);
 }
 
 function searchPage() {
